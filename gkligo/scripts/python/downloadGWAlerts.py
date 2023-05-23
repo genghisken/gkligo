18564caa877caae599fc00a26e591a0cddd26044
Ken Smith <k.w.smith@qub.ac.uk>
2023-05-24 00:36:04 +0100

    Added ability to write a meta file. Was originally going to get it to populate the database directly but this will make the daemon vary fragile.

--- gkligo/scripts/python/downloadGWAlerts.py
+++ gkligo/scripts/python/downloadGWAlerts.py
@@ -12,7 +12,7 @@ astropy
 mocpy
 
 Usage:
-  %s <configFile> <action> [--writeMap] [--writeMOC] [--directory=<directory>] [--contours=<contours>] [--pidfile=<pidfile>] [--logfile=<logfile>] [--daemonErrFile=<deamonErrFile>] [--daemonOutFile=<deamonOutFile>] [--terminal]
+  %s <configFile> <action> [--writeMap] [--writeMOC] [--directory=<directory>] [--contours=<contours>] [--pidfile=<pidfile>] [--logfile=<logfile>] [--daemonErrFile=<deamonErrFile>] [--daemonOutFile=<deamonOutFile>] [--terminal] [--writemeta] [--superevents]
   %s (-h | --help)
   %s --version
 
@@ -32,18 +32,22 @@ Options:
   --daemonErrFile=<daemonErrFile>   Daemon Error File - for recording unexpected errors [default: /tmp/ligodaemonerr.log].
   --daemonOutFile=<daemonOutFile>   Daemon Out File - for recording unexpected output [default: /tmp/ligodaemonout.log].
   --terminal                        Override the Daemon error and out files and write to the terminal. 
+  --writemeta                       Attempt to write the event meta into a file.
+  --superevents                     Only deal with superevents. Ignore Mock and Test events.
 
 E.g.:
   %s config.yaml start --directory=/home/atls/ligo --writeMap
   %s config.yaml start --writeMap --writeMOC --directory=/tmp --contours=90
   %s config.yaml start --writeMOC --directory=/tmp --contours=90,50,10
+  %s /home/ligo/maps_gkligo/config_downloads.yaml start --directory=/home/ligo/maps_gkligo --writeMap --writeMOC --writemeta --contours=90,50,10 --superevents
 
 """
 import sys
-__doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
+__doc__ = __doc__ % (sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0], sys.argv[0])
 from docopt import docopt
 
 from gcn_kafka import Consumer
+import yaml
 import json
 import base64
 from gkutils.commonutils import Struct, cleanOptions
@@ -116,6 +120,116 @@ def writeMOC(inputFilePointer, outputMOCName, contour, logger):
     logger.info('MOC file %s written' % outputMOCName)
 
 
+def writeMeta(options, dataDict, logger):
+    #import MySQLdb
+    from astropy.io import fits
+
+    eventid = dataDict['superevent_id']
+    url = dataDict['urls']['gracedb']
+    far = None
+    instruments = None
+
+    mjd = None
+    distance = None
+    distanceStd = None
+    classification = None
+    properties = None
+    significant = None
+    time = None
+    group = None
+    pipeline = None
+    alertType = dataDict['alert_type']
+    timeCreated = dataDict['time_created']
+
+    try:
+        time = dataDict['event']['time']
+    except KeyError as e:
+        logger.error("Can't find the time info.")
+
+    try:
+        group = dataDict['event']['group']
+    except KeyError as e:
+        logger.error("Can't find the group info.")
+
+    try:
+        pipeline = dataDict['event']['pipeline']
+    except KeyError as e:
+        logger.error("Can't find the pipeline info.")
+
+    try:
+        classification = dataDict['event']['classification']
+    except KeyError as e:
+        logger.error("Can't find the classification info.")
+
+    try:
+        significant = dataDict['event']['significant']
+    except KeyError as e:
+        logger.error("Can't find the significance info.")
+
+    try:
+        properties = dataDict['event']['properties']
+    except KeyError as e:
+        logger.error("Can't find the properties info.")
+
+    eventMeta = {}
+
+    try:
+        far = float(dataDict['event']['far'])
+        # years = 1/(far*(24*3600*365.2425))
+    except ValueError as e:
+        logger.error("Can't find the FAR info.")
+    except KeyError as e:
+        logger.error("Can't find the FAR info.")
+
+    try:
+        instruments = dataDict['event']['instruments']
+    except KeyError as e:
+        logger.error("Can't find the instuments info.")
+
+
+    # Some info (e.g. distance) only in the FITS file
+    h = fits.open(BytesIO(base64.b64decode(dataDict['event']['skymap'])))
+    header = h[1].header
+
+    try:
+        mjd = header['MJD-OBS']
+    except KeyError as e:
+        logger.error("The MJD-OBS variable is missing.")
+
+    try:
+        distance = header['DISTMEAN']
+    except KeyError as e:
+        logger.error("The DISTMEAN variable is missing.")
+
+    try:
+        distanceStd = header['DISTSTD']
+    except KeyError as e:
+        logger.error("The DISTSTD variable is missing.")
+
+    # Do NOT write to the database, which could potentially be locked and crash the
+    # daemon. Write another script (e.g the reports script) that does that. Just
+    # record the metadata for loading.
+
+    eventMeta = {'ALERT': {'event': {'far': far,
+                                     'instruments': instruments,
+                                     'significant': significant,
+                                     'classification': classification,
+                                     'properties': properties,
+                                     'time': time,
+                                     'group': group,
+                                     'pipeline': pipeline},
+                           'urls': {'gracedb': url},
+                           'superevent_id': eventid},
+                           'alert_type': alertType,
+                           'time_created': timeCreated,
+                 'HEADER': {'MJD-OBS': mjd,
+                            'DISTMEAN': distance,
+                            'DISTSTD': distanceStd},
+                }
+
+    return eventMeta
+
+ 
 def listen(options):
     """listen.
 
@@ -137,7 +251,6 @@ def listen(options):
 
     logger.addHandler(fh)
 
-    import yaml
     with open(options.configFile) as yaml_file:
         config = yaml.safe_load(yaml_file)
 
@@ -156,10 +269,30 @@ def listen(options):
             dataDict = json.loads(message.value().decode('utf-8'))
             try:
                 superEventId = dataDict['superevent_id']
+                eventType = superEventId[0] # M, T or S
+
                 alertTimeStamp = dataDict['time_created'].replace(' ','T')
                 alertType = dataDict['alert_type']
                 alertName = superEventId + '_' + alertType + '_' + alertTimeStamp
                 logger.info("Alert Received: %s" % alertName) # Future version of this script will log the output.
+
+                # Act on all events unless we only want superevents. Need to think about the logic!
+                if eventType != 'S' and options.superevents:
+                    logger.info("Skipping over this event.")
+                    # Skip to the next event
+                    continue
+
+                # Write the event meta into the databases
+                if dataDict['event'] is not None and options.writemeta:
+                    meta = writeMeta(options, dataDict, logger)
+                    if meta:
+                        for k,v in meta.items():
+                            logger.info("%s = %s" % (k, str(v)))
+                        # Overwrite the superevent info every time a new update arrives.
+                        with open(options.directory + '/' + superEventId + '.yaml', 'w') as yamlFile:
+                            yamlFile.write(yaml.dump(meta))
+
+
                 if dataDict['event'] is not None and options.writeMap:
                     skymap = dataDict['event']['skymap']
                     with open(options.directory + '/' + alertName + '.fits', 'wb') as fitsFile:
